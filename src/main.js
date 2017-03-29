89284df60ea03ce7e97d0d1a7c1e7e394928475f
designeng <denissavenok@gmail.com>
2017-03-29 18:57:10 +0300

    index -> main

---
+++ src/main.js
@@ -0,0 +1,55 @@
+import fs from 'fs';
+import _ from 'underscore';
+import when from 'when';
+import wire from 'essential-wire';
+import wireDebugPlugin from 'essential-wire/source/debug';
+
+import mongodb from 'mongodb';
+import { Readable } from 'stream';
+import Grid from 'gridfs-stream';
+
+import connectToDatabase from './plugins/connectToDatabase';
+import readFile from './plugins/readFile';
+
+let Promise = when.promise;
+
+const imagesDbUrl = 'mongodb://localhost:27017/images_storage';
+
+const spec = {
+    $plugins: [
+        // wireDebugPlugin,
+        connectToDatabase,
+        readFile,
+    ],
+
+    imagesDb: {
+        connectToDatabase: {
+            url:  imagesDbUrl
+        }
+    },
+
+    jpgFile: {
+        readFile: {
+            directory: __dirname + '/../images',
+            path: {$ref: 'one.jpg'}
+        }
+    },
+
+    // download: {
+    //     create: {
+    //         module: download,
+    //         args: [
+    //             items
+    //         ]
+    //     }
+    // },
+}
+
+wire(spec).then((context) => {
+    let { jpgFile } = context;
+    
+    console.log('IMAGE:::', jpgFile);
+    when(context.destroy()).then(() => {
+        process.exit();
+    })
+});
